ee598eba812b08eb6091b23128b06c15350a886f
michaeljymsgutierrez <michael@vielsoft.com>
2017-05-01 15:19:06 +0800

    Added function for adding items

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'highline/import'
 # Mysql 
 require 'mysql2'
 
-# admin accoount login function
+# admin accoount login function ===============================================================
 def admin_account_login()
   # prompt password function
   def password_prompt(message, mask='*')
@@ -24,7 +24,7 @@ def admin_account_login()
 
 end
 
-# show globally ivnvalid message
+# show globally ivnvalid message ===============================================================
 def invalid_message()
 	press = ""
 	puts "Invalid Input !!! \n\nPress ENTER to continue . . . "
@@ -32,32 +32,63 @@ def invalid_message()
 	main_program
 end
 
-# show invalid password
+# show invalid password ========================================================================
 def invalid_password()
 	puts "\n\nInvalid Password !!!\n\nPress ENTER to continue ..."
 	gets
 	main_program
 end
 
-# function for displaying menu admin
+# function for displaying menu admin ===========================================================
 def admin_menu()
 	system 'clear'
 	cmd  = ""
 	menu_choice = ""
 	puts "\n\n*** Welcome to Item Scheduling System *** \n\n"
-	puts "What would you like? Add, Update, Delete, View Items, View Schedule?"
+	puts "What would you like? (a) Manage Items,  (b) Manage Schedule?"
 	menu_choice = gets.chomp
 	
-	case menu_choice.upcase 
+	case menu_choice.upcase
+		when "A"
+			# call manage items function
+			manage_items
+		when "B"
 
-	when "ADD"
-
-		
+		else
+			# call invalid message
+			invalid_message
 	end
 
 end
 
-# Main System Function()
+
+# Manage Items function ========================================================================
+def manage_items()
+	system "clear"
+	cmd  = ""
+	menu_choice = ""
+	puts "\n\n*** Welcome to Item Scheduling System *** \n\n"
+	puts "What would you like?  (a)Add, (b)Update, (c)Delete , (d)View Items"
+	menu_choice = gets.chomp
+	
+	case menu_choice.upcase
+		when 'A'
+			item_name = ""
+			item_count = ""
+			puts "\nEnter Item Name:"
+			item_name = gets.chomp
+			puts "\nEnter Item Count:"
+			item_count = gets.chomp
+			$client.query("INSERT INTO items_tb (item_name, item_count) VALUES ('#{item_name}','#{item_count}')")
+			puts "\nSuccessfully Added \n\nPress Enter to continue . . ."
+			gets
+			# call manageg items
+			manage_items
+	end
+
+end
+
+# Main System Function() =======================================================================
 def main_program()
 	# better to see isolated program
 	system 'clear'
@@ -81,7 +112,7 @@ end
 
 
 
-# Make clinet variable  global
+# Make clinet variable  global ===================================================================
 $client = Mysql2::Client.new(:host => "localhost", :username => "root", :database => "ruby_db")
 $client.query("CREATE TABLE IF NOT EXISTS records_tb 
 			(id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, 
